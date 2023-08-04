bf56e66af4927e2b89610764fe43c48ff91503c9
JaySavani <jaduhacker2024@gmail.com>
2023-08-04 14:28:29 +0530

    test code against bigger input command

--- spacecraft .test.js
+++ spacecraft .test.js
@@ -38,4 +38,11 @@ describe('Spacecraft Navigation', () => {
         expect(chandrayaan.position_direction).toEqual({ x: 0, y: 1, z: 0, direction: 'D' });
     });
 
+    // check for bigger input  
+    it('should navigate the spacecraft accurately turnDown', () => {
+        const chandrayaan = new Spacecraft(0, 0, 0, 'N');
+        chandrayaan.executeCommands(['f', 'r', 'u', 'b', 'l']);
+        expect(chandrayaan.position_direction).toEqual({ x: 0, y: 1, z: -1, direction: 'W' });
+    });
+
 });
