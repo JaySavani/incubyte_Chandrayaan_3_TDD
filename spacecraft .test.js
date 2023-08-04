517922088208e865de2f54b1bc90c54f14a7622a
JaySavani <jaduhacker2024@gmail.com>
2023-08-04 13:26:28 +0530

    added code for move backward and all test case pass(including previous)

--- spacecraft .test.js
+++ spacecraft .test.js
@@ -1,8 +1,9 @@
 
 const Spacecraft = require("./spacecraft");
+
 describe('Spacecraft Navigation', () => {
     it('should navigate the spacecraft accurately forward', () => {
-        const chandrayaan = new Spacecraft();
+        const chandrayaan = new Spacecraft(); // // defult is (0,0,0,'N')
         chandrayaan.executeCommands(["f"]);
         expect(chandrayaan.position_direction).toEqual({ x: 0, y: 1, z: 0, direction: 'N' });
     });
@@ -12,5 +13,4 @@ describe('Spacecraft Navigation', () => {
         chandrayaan.executeCommands(["b"]);
         expect(chandrayaan.position_direction).toEqual({ x: 0, y: -1, z: 0, direction: 'N' });
     });
-
 });
--- spacecraft.js
+++ spacecraft.js
@@ -28,11 +28,39 @@ class Spacecraft {
         }
     }
 
+    moveBackward() {
+        switch (this.position_direction.direction) {
+            case "N":
+                this.position_direction.y -= 1;
+                break;
+            case "S":
+                this.position_direction.y += 1;
+                break;
+            case "E":
+                this.position_direction.x -= 1;
+                break;
+            case "W":
+                this.position_direction.x += 1;
+                break;
+            case "Up":
+                this.position_direction.z -= 1;
+                break;
+            case "Down":
+                this.position_direction.z += 1;
+                break;
+            default:
+                break;
+        }
+    }
+
     executeCommands(commands) {
         for (const command of commands) {
             switch (command) {
                 case "f":
                     this.moveForward();
+                    break;
+                case "b":
+                    this.moveBackward();
                     break;
                 default:
                     break;
