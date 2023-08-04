69853c2e89e495b115da53a78c5cc9f83bbb78d3
JaySavani <jaduhacker2024@gmail.com>
2023-08-04 13:16:02 +0530

    written code so spacecraft move forward accurately

--- spacecraft.js
+++ spacecraft.js
@@ -4,28 +4,28 @@ class Spacecraft {
     }
 
     moveForward() {
-        // switch (this.direction) {
-        //     case "N":
-        //         this.position.y += 1;
-        //         break;
-        //     case "S":
-        //         this.position.y -= 1;
-        //         break;
-        //     case "E":
-        //         this.position.x += 1;
-        //         break;
-        //     case "W":
-        //         this.position.x -= 1;
-        //         break;
-        //     case "Up":
-        //         this.position.z += 1;
-        //         break;
-        //     case "Down":
-        //         this.position.z -= 1;
-        //         break;
-        //     default:
-        //         break;
-        // }
+        switch (this.position_direction.direction) {
+            case "N":
+                this.position_direction.y += 1;
+                break;
+            case "S":
+                this.position_direction.y -= 1;
+                break;
+            case "E":
+                this.position_direction.x += 1;
+                break;
+            case "W":
+                this.position_direction.x -= 1;
+                break;
+            case "Up":
+                this.position_direction.z += 1;
+                break;
+            case "Down":
+                this.position_direction.z -= 1;
+                break;
+            default:
+                break;
+        }
     }
 
     executeCommands(commands) {
